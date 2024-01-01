ca435b0089d76d24d7ab979585dde91744856a4f
Chen Yang <2409248026@qq.com>
2024-01-01 20:43:40 +0800

    rm bin and build

--- build/CMakeFiles/sssp_merge_composite.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for sssp_merge_composite.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/wcc_merge_optimize_edgemap.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for wcc_merge_optimize_edgemap.
